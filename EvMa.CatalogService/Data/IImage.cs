4fdbe807e56cf1512794c202d29008291ed3d241
Oleksandr Mulyk <alex@mulyk.net>
2025-05-04 22:17:02 +0200

    Created interface and model for image

---
+++ EvMa.CatalogService/Data/IImage.cs
@@ -0,0 +1,13 @@
+﻿namespace EvMa.CatalogService.Data
+{
+    public interface IImage
+    {
+        public Guid ImageId { get; set; }
+
+        public string Url { get; set; }
+
+        public string AltText { get; set; }
+
+        public int Order { get; set; }
+    }
+}
---
+++ EvMa.CatalogService/Data/Image.cs
@@ -0,0 +1,14 @@
+﻿
+namespace EvMa.CatalogService.Data
+{
+    public class Image : IImage
+    {
+        public Guid ImageId { get; set; } = Guid.NewGuid();
+
+        public string Url { get; set; } = string.Empty;
+
+        public string AltText { get; set; } = string.Empty;
+
+        public int Order { get; set; } = 0;
+    }
+}
