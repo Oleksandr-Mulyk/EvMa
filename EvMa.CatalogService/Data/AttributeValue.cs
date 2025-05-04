657f15c219e12d830571343b211f999be789aa92
Oleksandr Mulyk <alex@mulyk.net>
2025-05-04 21:48:36 +0200

    Created interface and model for attribute value

---
+++ EvMa.CatalogService/Data/AttributeValue.cs
@@ -0,0 +1,19 @@
+﻿namespace EvMa.CatalogService.Data
+{
+    public class AttributeValue : IAttributeValue
+    {
+        public Guid AttributeValueId { get; set; } = Guid.NewGuid();
+
+        private Attribute _attribute { get; set; } = new Attribute();
+
+        public IAttribute Attribute
+        {
+            get => _attribute;
+            set => _attribute = (Attribute)value;
+        }
+
+        public object Product { get; set; } = new object();
+
+        public string Value { get; set; } = string.Empty;
+    }
+}
---
+++ EvMa.CatalogService/Data/IAttributeValue.cs
@@ -0,0 +1,13 @@
+﻿namespace EvMa.CatalogService.Data
+{
+    public interface IAttributeValue
+    {
+        public Guid AttributeValueId { get; set; }
+
+        public IAttribute Attribute { get; set; }
+
+        public object Product { get; set; }
+
+        public string Value { get; set; }
+    }
+}
