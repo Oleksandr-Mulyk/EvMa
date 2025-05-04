b858b6b8c0116a71788987bba81e03e2e9a00bab
Oleksandr Mulyk <alex@mulyk.net>
2025-05-04 22:25:44 +0200

    Created interface and model for price

---
+++ EvMa.CatalogService/Data/IPrice.cs
@@ -0,0 +1,17 @@
+﻿namespace EvMa.CatalogService.Data
+{
+    public interface IPrice
+    {
+        public Guid PriceId { get; set; }
+
+        public decimal Value { get; set; }
+
+        public decimal? MinQuantity { get; set; }
+
+        public decimal? MaxQuantity { get; set; }
+
+        public DateTime? StartAt { get; set; }
+
+        public DateTime? EndAt { get; set; }
+    }
+}
---
+++ EvMa.CatalogService/Data/Price.cs
@@ -0,0 +1,17 @@
+﻿namespace EvMa.CatalogService.Data
+{
+    public class Price : IPrice
+    {
+        public Guid PriceId { get; set; } = Guid.NewGuid();
+
+        public decimal Value { get; set; }
+
+        public decimal? MinQuantity { get; set; }
+
+        public decimal? MaxQuantity { get; set; }
+
+        public DateTime? StartAt { get; set; }
+
+        public DateTime? EndAt { get; set; }
+    }
+}
