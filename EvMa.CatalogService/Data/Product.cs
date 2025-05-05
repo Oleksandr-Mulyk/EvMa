fbfcc211143cc971d0379d88c2e9a134d66c68a2
Oleksandr Mulyk <alex@mulyk.net>
2025-05-05 19:27:35 +0200

    Changed product model for EF

--- EvMa.CatalogService/Data/Product.cs
+++ EvMa.CatalogService/Data/Product.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EvMa.CatalogService.Data
 {
     public class Product : IProduct
@@ -7,18 +9,20 @@ namespace EvMa.CatalogService.Data
 
         public string Sku { get; set; }
 
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
-        public string Description { get; set; }
+        public string Description { get; set; } = string.Empty;
 
-        public decimal Weight { get; set; }
+        public decimal Weight { get; set; } = 0m;
 
-        public (decimal Length, decimal Width, decimal Height) Dimensions { get; set; }
+        public (decimal Length, decimal Width, decimal Height) Dimensions { get; set; } = (0m, 0m, 0m);
 
-        public decimal RegularPrice { get; set; }
+        public decimal RegularPrice { get; set; } = 0m;
 
+        [Column("Prices")]
         private IList<Price>? _prices;
 
+        [NotMapped]
         public IList<IPrice>? Prices
         {
             get => (IList<IPrice>?)_prices;
@@ -27,32 +31,40 @@ namespace EvMa.CatalogService.Data
 
         public decimal StockQuantity { get; set; }
 
+        [Column("AttributeSet")]
         private AttributeSet _attributeSet;
 
+        [NotMapped]
         public IAttributeSet AttributeSet
         {
             get => _attributeSet;
             set => _attributeSet = (AttributeSet)value;
         }
 
+        [Column("AttributeValues")]
         private IList<AttributeValue>? _attributeValues;
 
+        [NotMapped]
         public IList<IAttributeValue> AttributeValues
         {
             get => (IList<IAttributeValue>?)_attributeValues;
             set => _attributeValues = value?.Cast<AttributeValue>().ToList();
         }
 
+        [Column("Category")]
         private Category _category;
 
+        [NotMapped]
         public ICategory Category
         {
             get => _category;
             set => _category = (Category)value;
         }
 
+        [Column("Images")]
         private IList<Image>? _images;
 
+        [NotMapped]
         public IList<IImage>? Images
         {
             get => (IList<IImage>?)_images;
