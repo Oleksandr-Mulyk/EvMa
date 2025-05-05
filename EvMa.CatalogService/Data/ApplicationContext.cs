f8207e0d680f0417721a7a2a4f36da50bf6debd7
Oleksandr Mulyk <alex@mulyk.net>
2025-05-05 19:17:17 +0200

    Created db context

---
+++ EvMa.CatalogService/Data/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EvMa.CatalogService.Data
+{
+    public class ApplicationContext(DbContextOptions<ApplicationContext> options) : DbContext(options)
+    {
+        public DbSet<Image> Images { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Attribute> Attributes { get; set; }
+
+        public DbSet<AttributeSet> AttributeSets { get; set; }
+
+        public DbSet<AttributeValue> AttributeValues { get; set; }
+
+        public DbSet<Price> Prices { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
