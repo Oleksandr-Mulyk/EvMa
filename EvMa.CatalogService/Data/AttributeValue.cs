8c2af6191f1f07e77e0d8142a2e2da294949afe9
Oleksandr Mulyk <alex@mulyk.net>
2025-05-05 19:21:42 +0200

    Changed attribute value model for EF

--- EvMa.CatalogService/Data/AttributeValue.cs
+++ EvMa.CatalogService/Data/AttributeValue.cs
@@ -1,19 +1,25 @@
-﻿namespace EvMa.CatalogService.Data
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EvMa.CatalogService.Data
 {
     public class AttributeValue : IAttributeValue
     {
         public Guid AttributeValueId { get; set; } = Guid.NewGuid();
 
+        [Column("Attribute")]
         private Attribute _attribute { get; set; } = new Attribute();
 
+        [NotMapped]
         public IAttribute Attribute
         {
             get => _attribute;
             set => _attribute = (Attribute)value;
         }
 
+        [Column("Product")]
         private Product _product { get; set; } = new Product();
 
+        [NotMapped]
         public IProduct Product
         {
             get => _product;
