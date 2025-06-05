682795f39d90c461c7c1c38437d22f120e60ca00
Oleksandr Mulyk <alex@mulyk.net>
2025-06-05 17:00:16 +0200

    Created base repository

---
+++ EvMa.CatalogService/Data/Repositories/DbContextRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EvMa.CatalogService.Data.Repositories
+{
+    public class DbContextRepository<T, TClass, TId>(DbSet<TClass> dbSet, ApplicationContext dbContext)
+        : IRepository<T, TId> where T : class where TClass : class, T
+    {
+        public virtual async Task<T?> GetByIdAsync(TId id) =>
+            await dbSet.FindAsync(id) ?? throw new Exception(NotFoundMessage);
+
+        public virtual IQueryable<T> GetAll() => dbSet.AsQueryable().Select(item => item as T);
+
+        public virtual async Task<T> AddAsync(T entity)
+        {
+            var result = await dbSet.AddAsync(entity as TClass ?? throw new InvalidCastException());
+
+            if (result.State == EntityState.Added)
+            {
+                await dbContext.SaveChangesAsync();
+                return result.Entity;
+            }
+
+            throw new Exception(NotAddedMessage);
+        }
+
+        public virtual async Task<T> UpdateAsync(T entity)
+        {
+            var result = dbSet.Update(entity as TClass ?? throw new InvalidCastException());
+
+            if (result.State == EntityState.Modified)
+            {
+                await dbContext.SaveChangesAsync();
+                return result.Entity;
+            }
+
+            throw new Exception(NotUpdatedMessage);
+        }
+
+        public virtual async Task DeleteAsync(T entity)
+        {
+            var result = dbSet.Remove(entity as TClass ?? throw new InvalidCastException());
+
+            if (result.State == EntityState.Deleted)
+            {
+                await dbContext.SaveChangesAsync();
+            }
+            else
+            {
+                throw new Exception(NotDeletedMessage);
+            }
+        }
+
+        protected virtual string NotFoundMessage => $"{typeof(TClass).Name} not found";
+
+        protected virtual string NotAddedMessage => $"{typeof(TClass).Name} not added";
+
+        protected virtual string NotUpdatedMessage => $"{typeof(TClass).Name} not updated";
+
+        protected virtual string NotDeletedMessage => $"{typeof(TClass).Name} not deleted";
+    }
+
+    public class DbContextRepository<T, TClass>(DbSet<TClass> dbSet, ApplicationContext dbContext) :
+        DbContextRepository<T, TClass, Guid>(dbSet, dbContext), IRepository<T>
+        where T : class where TClass : class, T { }
+}
---
+++ EvMa.CatalogService/Data/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace EvMa.CatalogService.Data.Repositories
+{
+    public interface IRepository<T, TId> where T : class
+    {
+        public Task<T?> GetByIdAsync(TId id);
+
+        public IQueryable<T> GetAll();
+
+        public Task<T> AddAsync(T entity);
+
+        public Task<T> UpdateAsync(T entity);
+
+        public Task DeleteAsync(T entity);
+    }
+
+    public interface IRepository<T> : IRepository<T, Guid> where T : class { }
+}
+    
