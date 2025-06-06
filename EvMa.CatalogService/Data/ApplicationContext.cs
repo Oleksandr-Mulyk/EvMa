6ce28d4d8f14898676ec838fdeda5ccde7ffdc7e
Oleksandr Mulyk <alex@mulyk.net>
2025-06-06 16:11:31 +0200

    Fixed application context

--- EvMa.CatalogService/Data/ApplicationContext.cs
+++ EvMa.CatalogService/Data/ApplicationContext.cs
@@ -1,5 +1,4 @@
-﻿using EvMa.CatalogService.Data.ApplicationModels;
-using EvMa.CatalogService.Data.Configurations;
+﻿using EvMa.CatalogService.Data.Configurations;
 using EvMa.CatalogService.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,17 +8,17 @@ namespace EvMa.CatalogService.Data
     {
         public DbSet<Image> Images { get; set; }
 
-        public DbSet<AppCategory> Categories { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         public DbSet<ProductAttribute> Attributes { get; set; }
 
-        public DbSet<AppAttributeSet> AttributeSets { get; set; }
+        public DbSet<AttributeSet> AttributeSets { get; set; }
 
-        public DbSet<AppAttributeValue> AttributeValues { get; set; }
+        public DbSet<AttributeValue> AttributeValues { get; set; }
 
         public DbSet<Price> Prices { get; set; }
 
-        public DbSet<AppProduct> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
