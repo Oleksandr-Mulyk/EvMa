89293e9becbb46fdfee0f564a1329976a2bbef6d
Oleksandr Mulyk <alex@mulyk.net>
2025-07-08 07:42:00 +0200

    Removed extra projects

--- EvMa.AppHost/Program.cs
+++ EvMa.AppHost/Program.cs
@@ -2,13 +2,4 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-var apiService = builder.AddProject<Projects.EvMa_ApiService>("apiservice");
-
-builder.AddProject<Projects.EvMa_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(cache)
-    .WaitFor(cache)
-    .WithReference(apiService)
-    .WaitFor(apiService);
-
 builder.Build().Run();
