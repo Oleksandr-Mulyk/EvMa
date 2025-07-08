b079968f9777ddf37be7448642483e320c22c3b1
Oleksandr Mulyk <alex@mulyk.net>
2025-07-08 13:07:09 +0200

    Configured grpc clients

---
+++ EvMa.Web.Admin/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using EvMa.Web.Admin.Protos;
+
+namespace EvMa.Web.Admin.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddGrpcClients(this IServiceCollection services, WebApplicationBuilder? builder)
+        {
+            services.AddGrpcClient<CategoryService.CategoryServiceClient>(options =>
+            {
+                options.Address = new Uri(builder.Configuration["evma-catalogservice"]);
+            });
+
+            services.AddGrpcClient<AttributeSetService.AttributeSetServiceClient>(options =>
+            {
+                options.Address = new Uri(builder.Configuration["evma-catalogservice"]);
+            });
+
+            services.AddGrpcClient<ProductService.ProductServiceClient>(options =>
+            {
+                options.Address = new Uri(builder.Configuration["evma-catalogservice"]);
+            });
+
+            return services;
+        }
+    }
+}
--- EvMa.Web.Admin/Program.cs
+++ EvMa.Web.Admin/Program.cs
@@ -1,4 +1,5 @@
 using EvMa.Web.Admin.Components;
+using EvMa.Web.Admin.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,20 +9,19 @@ builder.AddServiceDefaults();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddGrpcClients(builder);
+
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 
 app.UseAntiforgery();
 
