4e57adad2d41c903bb28abb69b6379c6b138ff4f
Oleksandr Mulyk <alex@mulyk.net>
2026-02-13 19:01:22 +0100

    Provision core infrastructure in AppHost

--- src/Orchestration/EvMa.AppHost/EvMa.AppHost.AppHost/AppHost.cs
+++ src/Orchestration/EvMa.AppHost/EvMa.AppHost.AppHost/AppHost.cs
@@ -1,3 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder.AddPostgres("postgres")
+    .WithImageTag("latest")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var customersDb = postgres.AddDatabase("customersdb");
+
+var cache = builder.AddRedis("cache");
+
+var messaging = builder.AddRabbitMQ("messaging")
+    .WithManagementPlugin();
+
 builder.Build().Run();
